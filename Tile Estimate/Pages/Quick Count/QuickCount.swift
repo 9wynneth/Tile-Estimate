001fa7f3327951e0d225a57881284d66b29fdf8c
9wynneth <gwynnethgwen10@gmail.com>
2024-04-28 11:00:16 +0700

    popup results

---
+++ Tile Estimate/Pages/Quick Count/QuickCount.swift
@@ -0,0 +1,18 @@
+//
+//  QuickCount.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 27/04/24.
+//
+
+import SwiftUI
+
+struct QuickCount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuickCount()
+}
---
+++ Tile Estimate/Pages/Result/popupResult.swift
@@ -0,0 +1,18 @@
+//
+//  popupResult.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 28/04/24.
+//
+
+import SwiftUI
+
+struct popupResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    popupResult()
+}
