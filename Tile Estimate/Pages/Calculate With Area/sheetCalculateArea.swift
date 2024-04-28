055a3eabcb8281d91cc77682acb3891060cba23f
9wynneth <gwynnethgwen10@gmail.com>
2024-04-28 18:46:35 +0700

    sheets calculating using area

---
+++ Tile Estimate/Pages/Calculate With Area/sheetCalculateArea.swift
@@ -0,0 +1,18 @@
+//
+//  sheetCalculateArea.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 28/04/24.
+//
+
+import SwiftUI
+
+struct sheetCalculateArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sheetCalculateArea()
+}
