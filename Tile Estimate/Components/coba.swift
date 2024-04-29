0f128cebe2fe331c6381aadcbb87782822c2b266
9wynneth <gwynnethgwen10@gmail.com>
2024-04-29 15:50:12 +0700

    pickerdropdown almost done

---
+++ Tile Estimate/Components/coba.swift
@@ -0,0 +1,18 @@
+//
+//  coba.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 28/04/24.
+//
+
+import SwiftUI
+
+struct coba: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    coba()
+}
---
+++ Tile Estimate/Components/dropdownbtnList.swift
@@ -0,0 +1,18 @@
+//
+//  dropdownbtnList.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 29/04/24.
+//
+
+import SwiftUI
+
+struct dropdownbtnList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dropdownbtnList()
+}
