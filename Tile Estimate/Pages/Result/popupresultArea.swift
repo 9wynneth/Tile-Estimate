96a7ac9bbdf6fb57b5fa95fd2bda6c8c1ddda3ca
9wynneth <gwynnethgwen10@gmail.com>
2024-05-04 22:01:56 +0700

    sheet calculate with area (calculation) done

---
+++ Tile Estimate/Pages/Result/popupresultArea.swift
@@ -0,0 +1,18 @@
+//
+//  popupresultArea.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 04/05/24.
+//
+
+import SwiftUI
+
+struct popupresultArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    popupresultArea()
+}
