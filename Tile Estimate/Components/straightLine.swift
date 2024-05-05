f5b92d3db255168fd5ce377160e05f38a8310b7d
9wynneth <gwynnethgwen10@gmail.com>
2024-05-05 19:51:45 +0700

    history bisa display data stored

---
+++ Tile Estimate/Components/straightLine.swift
@@ -0,0 +1,18 @@
+//
+//  straightLine.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 05/05/24.
+//
+
+import SwiftUI
+
+struct straightLine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    straightLine()
+}
