b0be9db019641735b279962d335bce8f19c1467e
9wynneth <gwynnethgwen10@gmail.com>
2024-05-05 21:19:32 +0700

    history done

--- Tile Estimate/Components/textBody14.swift
+++ Tile Estimate/Components/textBody14.swift
@@ -1,5 +1,5 @@
 //
-//  straightLine.swift
+//  textBody14.swift
 //  Tile Estimate
 //
 //  Created by Gwynneth Isviandhy on 05/05/24.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct straightLine: View {
+struct textBody14: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    straightLine()
+    textBody14()
 }
