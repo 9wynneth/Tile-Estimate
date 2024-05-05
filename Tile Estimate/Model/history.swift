fa24fcca62efe03a875b7338d9b714c7c0bc3a19
9wynneth <gwynnethgwen10@gmail.com>
2024-05-05 11:44:14 +0700

    calculation correct, keyboard sdh ga ngeblock

---
+++ Tile Estimate/Model/history.swift
@@ -0,0 +1,18 @@
+//
+//  history.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 04/05/24.
+//
+
+import SwiftUI
+
+struct history: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    history()
+}
---
+++ Tile Estimate/Pages/History/historyList.swift
@@ -0,0 +1,18 @@
+//
+//  historyList.swift
+//  Tile Estimate
+//
+//  Created by Gwynneth Isviandhy on 04/05/24.
+//
+
+import SwiftUI
+
+struct historyList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    historyList()
+}
